b1e310c526054ef303747e5425c87a558968d06d
patrick <paddel10@hotmail.com>
2017-06-13 22:46:59 +0200

    add new label creator

--- utils/createLabels.sh
+++ utils/createLabels.sh
@@ -3,3 +3,5 @@
 COLOR=$1
 NUMBER=$2
 convert -background "${COLOR}" -fill white  -font Helvetica -size 48x48  -pointsize 24  -gravity center label:${NUMBER} T${NUMBER}.png
+
+# convert -bordercolor black -border 1x1 -background "white" -fill black -font Helvetica -size 46x46  -pointsize 24  -gravity center label:S24 S24.png
