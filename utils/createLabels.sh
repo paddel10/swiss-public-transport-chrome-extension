65c891794056ca83f077160511a8870e80d6cc31
patrick <paddel10@hotmail.com>
2017-06-28 21:11:20 +0200

    script for batch processing

--- utils/createLabels.sh
+++ utils/createLabels.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-COLOR=$1
-NUMBER=$2
-convert -background "${COLOR}" -fill white  -font Helvetica -size 48x48  -pointsize 24  -gravity center label:${NUMBER} T${NUMBER}.png
+# batch
+# cat zvv.txt | while read line; do sh createLabels.sh $line; done
 
-# convert -bordercolor black -border 1x1 -background "white" -fill black -font Helvetica -size 46x46  -pointsize 24  -gravity center label:S24 S24.png
+TRANSPORT=$1
+NUMBER=$2
+COLOR=$3
+
+convert -background "${COLOR}" -fill white  -font Helvetica -size 48x48  -pointsize 24  -gravity center label:${NUMBER} ${TRANSPORT}${NUMBER}.png
